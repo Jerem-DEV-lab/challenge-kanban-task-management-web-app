5096250c2641978a76ba0dc0455581516937931c
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2023-01-23 15:39:20 +0100

    Chore: Setup css and store assets

--- apps/kanban-task-management/src/pages/_document.tsx
+++ apps/kanban-task-management/src/pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="fr">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="fr">
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={""}/>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@500;700&display=swap"
+                        rel="stylesheet"/>
+                    <link rel="shortcut icon" href="/favicon-32x32.png"/>
+            </Head>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+)
 }
