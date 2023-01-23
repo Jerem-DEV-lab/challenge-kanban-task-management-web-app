0f5d8fc11fe8679ddb3386ee8c36473246792ac4
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2023-01-23 14:58:24 +0100

    Chore: Clean code

--- apps/kanban-task-management/next.config.js
+++ apps/kanban-task-management/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@kanban/ui', '@kanban/typescript'],
 }
 
 module.exports = nextConfig
